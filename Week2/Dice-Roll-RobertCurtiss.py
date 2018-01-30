0fd6b620e4bb6cfda2a3338bb395185479c49606
Robert Curtiss <me@robertcurtiss.com>
2018-01-30 15:50:40 -0500

    Updated description

--- Week2/Dice-Roll-RobertCurtiss.py
+++ Week2/Dice-Roll-RobertCurtiss.py
@@ -5,7 +5,8 @@ Class: CSCI-1511
 FILE: Dice-Roll-RobertCurtiss.py
 Created: 1/30/2018
 Project: Week2
-Description:
+Description: Roll the dice and sum up the points use
+    an appropriate term to describe the roll
 
 """
 
