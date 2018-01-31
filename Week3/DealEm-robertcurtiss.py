36f1d732970640ee97b5a6865a062cb7dd55d717
Robert Curtiss <me@robertcurtiss.com>
2018-01-31 08:36:13 -0500

    Added week 1 docs directory

---
+++ Week3/DealEm-robertcurtiss.py
@@ -0,0 +1,14 @@
+"""
+Author: Robert W. Curtiss
+
+Class: CSCI-1511
+ -Project: CSCI1511
+ -File: DealEm-robertcurtiss
+ -Created: Jan, 31, 2018
+Description:
+    Asks the user for the number of players and the number of cards in each hand.
+    Then, displays all hands of cards from a deck of playing cards
+    using the same format as described in Project 3.
+
+"""
+
---
+++ Week3/WordJumble-robertcurtiss.com.py
@@ -0,0 +1,12 @@
+"""
+Author: Robert W. Curtiss
+
+Class: CSCI-1511
+ -Project: CSCI1511
+ -File: WordJumble-robertcurtiss.com
+ -Created: Jan, 31, 2018
+Description:
+    gets a message from the user,
+    and then prints it out backwards (pg. 120, #2).
+"""
+
