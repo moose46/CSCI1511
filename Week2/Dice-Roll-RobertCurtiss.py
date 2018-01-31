f11e9bb9350f2ffc7968fa94aa3df9ddd93783ce
Robert Curtiss <me@robertcurtiss.com>
2018-01-30 21:10:12 -0500

    Completed round 3 of testing git

--- Week2/Dice-Roll-RobertCurtiss.py
+++ Week2/Dice-Roll-RobertCurtiss.py
@@ -6,7 +6,7 @@ FILE: Dice-Roll-RobertCurtiss.py
 Created: 1/30/2018
 Project: Week2
 Description: Roll the dice and sum up the points use
-    an appropriate term to describe the roll!!
+    an appropriate term to describe the roll!
 
 """
 
