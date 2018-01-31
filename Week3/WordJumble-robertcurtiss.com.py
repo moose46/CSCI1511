a107287618f15d863984d5223ad4e20eb3f23a02
Robert Curtiss <me@robertcurtiss.com>
2018-01-31 09:59:38 -0500

    Updated Jumble program

--- Week3/WordJumble-robertcurtiss.com.py
+++ Week3/WordJumble-robertcurtiss.com.py
@@ -6,7 +6,35 @@ Class: CSCI-1511
  -File: WordJumble-robertcurtiss.com
  -Created: Jan, 31, 2018
 Description:
-    gets a message from the user,
-    and then prints it out backwards (pg. 120, #2).
+    The computer picks a random word and then jumbles it.
+    The player has to guess the original word
 """
 
+import random as rnd
+
+WORDS = ("water", "snow", "ice", "river", "lake", "pond")
+
+randomWord = rnd.choice(WORDS)
+# save the original word for later
+savedWord = randomWord
+jumbledWord = ""
+
+while randomWord:
+    position = rnd.randrange(len(randomWord))
+    jumbledWord += randomWord[position]
+    randomWord = randomWord[:position] + randomWord[(position + 1):]
+
+print("""\tWelcome to the world of jumble!
+\tUnscramble the letter to make a word:\n""")
+
+
+print("%s %s" % (savedWord, jumbledWord))
+
+guess = input("Your guess: ")
+while guess != savedWord and guess != "":
+    print("Sorry that's not correct!")
+    guess = input("Your guess: ")
+
+print("You guessed it, congratulations!")
+print("Thanks for playing!")
+# input("Press enter to exit:")
