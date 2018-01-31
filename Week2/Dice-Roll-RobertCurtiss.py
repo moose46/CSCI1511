34f34cafaac08631d5548e8a04d38b42a046034a
Robert Curtiss <me@robertcurtiss.com>
2018-01-30 19:17:08 -0500

    Completed round one of testing

--- Week2/Dice-Roll-RobertCurtiss.py
+++ Week2/Dice-Roll-RobertCurtiss.py
@@ -12,6 +12,36 @@ Description: Roll the dice and sum up the points use
 
 import random as rnd
 
+rnd.seed(rnd.randint(100, 3000))
+print("Welcome to Roll the Dice game!")
+
+termsDict = {"1,1": "Snake Eyes",
+             "1,2": "Ace Caught a Duce",
+             "2,2": "Little Joe from Kokomo",
+             "2,3": "Little Phoebe",
+             "3,3": "Jimmy Hicks from the Sticks",
+             "1,6": "Six Ace",
+             "4,4": "Eighter from Decatur",
+             "4,5": "Nina from Pasadena",
+             "5,5": "Puppy Paws",
+             "5,6": "Six Five no Jive",
+             "6,6": "Boxcars"
+             }
+dice = [0, 0]
+rnd.seed()
 while True:
-    theRoll = rnd.randint(2, 12)
-    break
+    dice[0] = rnd.randint(1, 6)
+    dice[1] = rnd.randint(1, 6)
+    dice.sort()
+    theDots = str(dice[0]) + "," + str(dice[1])
+
+    print("The roll: (%d,%d) --- Total Points: (%d)" % (dice[0], dice[1], dice[0] + dice[1]), end="")
+
+    if theDots in termsDict:
+        print(" --- (%s)" % termsDict[str(dice[0]) + "," + str(dice[1])])
+
+    ans = input("\nPlay again (y): ")
+    if ans == 'y':
+        continue
+    else:
+        break
