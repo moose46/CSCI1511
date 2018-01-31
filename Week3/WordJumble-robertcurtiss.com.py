dc64a05df2308d7eb05a01351a127250cedbcac4
Robert Curtiss <me@robertcurtiss.com>
2018-01-31 11:03:34 -0500

    Added a hint to jumble

--- Week3/WordJumble-robertcurtiss.com.py
+++ Week3/WordJumble-robertcurtiss.com.py
@@ -18,6 +18,7 @@ randomWord = rnd.choice(WORDS)
 # save the original word for later
 savedWord = randomWord
 jumbledWord = ""
+score = 100
 
 while randomWord:
     position = rnd.randrange(len(randomWord))
@@ -27,13 +28,18 @@ while randomWord:
 print("""\tWelcome to the world of jumble!
 \tUnscramble the letter to make a word:\n""")
 
-
 print("%s %s" % (savedWord, jumbledWord))
 
 guess = input("Your guess: ")
 while guess != savedWord and guess != "":
+    hint = ""
     print("Sorry that's not correct!")
+    hint = input("Would you like a hint? (y): ")
+    if hint != "":
+        positionHint = rnd.randrange(len(savedWord))
+        print("Word Letter [%s] is an %s " % (positionHint + 1, savedWord[positionHint]))
     guess = input("Your guess: ")
+    score -= 1
 
 print("You guessed it, congratulations!")
 print("Thanks for playing!")
