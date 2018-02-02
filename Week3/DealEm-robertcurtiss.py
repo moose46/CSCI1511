ffe3fc1456c5270275fea9f865e4e0fc2e0d1f0a
Robert Curtiss <me@robertcurtiss.com>
2018-02-01 20:36:59 -0500

    Started the code

--- Week3/DealEm-robertcurtiss.py
+++ Week3/DealEm-robertcurtiss.py
@@ -12,3 +12,29 @@ Description:
 
 """
 
+# init the deck of cards
+suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+cards = []
+
+# create the deck of cards with all four suits
+for suit in suits:
+	cards.append([(1, "1 ", suit),
+				  (2, "2 ", suit),
+				  (3, "3 ", suit),
+				  (4, "4 ", suit),
+				  (5, "5 ", suit),
+				  (6, "6 ", suit),
+				  (7, "7 ", suit),
+				  (8, "8 ", suit),
+				  (9, "9 ", suit),
+				  (10, "10 ", suit),
+				  (10, "J ", suit),
+				  (10, "Q ", suit),
+				  (10, "K ", suit),
+				  (11, "A ", suit)])
+
+for suit in cards:
+	for card in suit:
+		print(card[1],card[2])
+
+print("\nPress enter to continue: ")
