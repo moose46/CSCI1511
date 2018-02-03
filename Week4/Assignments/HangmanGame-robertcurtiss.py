1f2d9bf9aa9236f25b8b98c880c4493c3b07879a
Robert Curtiss <me@robertcurtiss.com>
2018-02-02 19:33:15 -0500

    Hangman with try catch

---
+++ Week4/Assignments/HangmanGame-robertcurtiss.py
@@ -0,0 +1,46 @@
+"""
+Author: Robert W. Curtiss
+
+Class: CSCI-1511
+ -Project: CSCI1511
+ -File: HangmanGame-robertcurtiss
+ -Created: Feb, 02, 2018
+Description:
+	Hangman Game, guess the word or you are done!
+"""
+
+import random as rnd
+
+WORDS = ["KANGAROO","ELEPHANT", "MONGOOSE","RACCOON","OSTRICH","HORSE"]
+
+word = rnd.choice(WORDS)
+
+dashes = "_ " * len(word)
+guessedLetters = []
+
+wordList = list(word)
+print(word)
+print(dashes)
+while True:
+	try:
+		letter =  input("\nYour Guess: ").upper()
+		if letter in guessedLetters:
+			print("You already have used that letter, try again:")
+			continue
+		break
+	except:
+		pass
+	finally:
+		dashes = ""
+
+	for i,j in enumerate(wordList):
+		if j == letter:
+			guessedLetters.append(letter)
+			dashes += letter + " "
+			#print(l,i)
+		else:
+			dashes += "_ "
+	print(dashes)
+#print(guessedLetters)o
+
+#input("\nPress enter to continue: ")
