c748eb98129cacef35e717e6601d82a264d4ac4d
Robert Curtiss <me@robertcurtiss.com>
2018-02-03 15:32:42 -0500

    Just checking in

--- Week4/Assignments/HangmanGame-robertcurtiss.py
+++ Week4/Assignments/HangmanGame-robertcurtiss.py
@@ -12,35 +12,37 @@ Description:
 import random as rnd
 
 WORDS = ["KANGAROO", "ELEPHANT", "MONGOOSE", "RACCOON", "OSTRICH", "HORSE"]
-
 word = rnd.choice(WORDS)
+# init status as a list with dashes
+status = "_ " * len(word)
 
-#init status as a list with dashes
-status = ["_ "] * len(word)
-
-#make guessed letters a list
+# make guessed letters a list
 guessedLetters = []
-#make word a list
+# make word a list
 wordList = list(word)
 print(word)
-for i,j in enumerate(status):
-	print(status[i],end="")
-while True:
-	letter = input("\nYour Guess: ").upper()
-	if letter in guessedLetters:
-		print("You already have used that letter, try again:")
-		continue
-	for i, j in enumerate(wordList):
-		if j == letter:
-			guessedLetters.append(letter)
-	for i, j in enumerate(status):
-		if wordList[i] == letter:
-			status[i] = letter + " "
-			continue
-		if letter in guessedLetters and letter == j:
-			status[i] += letter + " "
-	for i, j in enumerate(status):
-		print(status[i], end="")
-# print(guessedLetters)o
-
-# input("\nPress enter to continue: ")
+print(status, end=" ")
+# for i, j in enumerate(status):
+#     print(status[i], end="")
+noMatch = True
+while noMatch:
+    foundLetters = 0
+    status = ""
+    letter = input("\nYour Guess: ").upper()
+    if letter in guessedLetters:
+        print("You already have used that letter, try again:")
+        continue
+    for l in wordList:
+        if letter not in guessedLetters:
+            guessedLetters.append(letter)
+        if l in guessedLetters :
+            status += l + " "
+            continue
+        else:
+            status += "_ "
+    print(status)
+    #test for exiting the loop
+    if set(wordList).difference(guessedLetters) == set():
+        print("You guessed it!")
+        noMatch = False
+# input("\nGame over Press enter to continue: ")
