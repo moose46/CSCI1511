8baed3b2ec02359a6b190a8cd560a10bb3abf93c
Robert Curtiss <me@robertcurtiss.com>
2018-02-04 20:54:49 -0500

    Completed selection sort

--- Week4/Assignments/selection-sort-robertcurtiss.py
+++ Week4/Assignments/selection-sort-robertcurtiss.py
@@ -5,6 +5,27 @@ Class: CSCI-1511
  -Project: CSCI1511
  -File: selection-sort-robertcurtiss
  -Created: Feb, 04, 2018
-Description:
+Description: Selection sort on an unordered list
 
 """
+
+import  random as rnd
+
+unOrderd = list(range(10))
+
+ordered = []
+
+rnd.shuffle(unOrderd)
+
+print(unOrderd)
+
+
+while len(unOrderd) > 0:
+	lowest = 10
+	for num in unOrderd:
+		if num < lowest:
+			lowest = num
+	unOrderd.remove(lowest)
+	ordered.append(lowest)
+
+print(ordered)
