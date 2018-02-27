b90b5cd09f6d874f30b88b6b0d86ed37a91b84c8
me <me@robertcurtiss.com>
2018-02-27 16:50:45 -0500

    Again

--- Week6/Assignments/phone-dir-robertcurtiss.py
+++ Week6/Assignments/phone-dir-robertcurtiss.py
@@ -1,3 +1,13 @@
+"""
+Author: Robert W. Curtiss
+
+Class: CSCI-1511
+ -Project: CSCI1511
+ -File: phone-dir-robertcurtiss.py
+ -Created: Feb, 20th, 2018
+Description:
+creates and updates a phone directory
+"""
 import shelve as sh
 
 the_phone_book_name = "thePhoneBook"
@@ -6,6 +16,7 @@ the_phone_book_name = "thePhoneBook"
 class Person():
     name = ""
     phone = ""
+    new = False
 
     def __init__(self, name='XX', phone='YY'):
         self.name = name
@@ -16,15 +27,17 @@ class Person():
 
 
 def print_error():
+    """prints an error message"""
     print("Incorrect Selection, try again!\n")
 
 
 def show_menu():
+    """displays an menu of functions for maintaining the database"""
     while True:
         try:
             display_menu()
             ans = int(input("Enter your selection [1-4]: "))
-            if ans >= 1 and ans <= 5:
+            if ans in range(1, 6):
                 return ans
             raise ValueError()
         except Exception as Ex:
@@ -33,35 +46,40 @@ def show_menu():
 
 
 def display_menu():
+    """display menu options"""
     print("""Menu Selection:
     1. Add Entry
-    2. Delete Entry
-    3. Look Up Entry
-    4. Show All Entries
-    5. Exit\n""")
+    2. Update Entry
+    3. Delete Entry
+    4. Look Up Entry
+    5. Show All Entries
+    6. Exit\n""")
 
 
 def create_default_data():
+    """some data to play with during testing, stomps on database every time"""
     db = sh.open(the_phone_book_name, flag='n', writeback=True)
     phone_list = {'Bob': '6145617554',
                   'Leslie': '3095551212',
                   'Carol': '61455551212',
                   'Rick': '8955551212'}
 
-    for k in phone_list:
-        db[k] = phone_list[k]
+    for name_key in phone_list:
+        db[name_key] = phone_list[name_key]
     db.sync()
     db.close()
 
 
-def get_new_entry():
+def get_new_entry(in_person):
+    """collects and assigns data to a new person and return the person"""
     person = Person()
-    person.name = get_name()
+    person.name = get_name(person)
     person.phone = input("Enter Phone Number: ")
     return person
 
 
 def delete_entry(key):
+    """delete an entry from the database"""
     db = sh.open(the_phone_book_name, flag='c', writeback=True)
     if key in db:
         confirm = input("Delete {name} [y/n]: ".format(name=key))
@@ -71,22 +89,39 @@ def delete_entry(key):
 
 
 def db_add_entry(person):
+    """either updates an existing entry or adds a new entry"""
     db = sh.open(the_phone_book_name, flag='c', writeback=True)
     if person.name in db:
-        print("Updating entry ..... {name}\n".format(name=person.name))
-
-
+        print("Updating existing entry ..... {name}\n".format(name=person.name))
+    else:
+        person.new = True
+        print("Adding new entry ..... {name}".format(name=person.name))
     db[person.name.capitalize()] = person.phone
     db.sync()
     db.close()
     db_show_all()
+
+
+def db_update_entry():
+    """updates an existing entry"""
+    db = sh.open(the_phone_book_name, flag='c', writeback=True)
+    name = get_name()
+    if name in db:
+        phone_number = get_phone_number(db[name.capitalize()])
+        print("Updating existing entry ..... {name}\n".format(name=name))
+        db[name.capitalize()] = phone_number
+        db.sync()
+    else:
+        print_error()
+    db.close()
+    db_show_all()
+
 
 def find_entry(key):
     """Find a entry by phone number or name"""
     found_list = []
     db = sh.open(the_phone_book_name, flag='c', writeback=True)
     for k in db:
-
         name = str(k).lower()
         phone = str(db[k])
         if (name.find(key.lower())) >= 0 or (phone.find(key.lower()) >= 0):
@@ -95,11 +130,11 @@ def find_entry(key):
             person.phone = db[k]
             found_list.append(person)
     display_list(found_list)
-
     db.close()
 
 
 def display_list(the_list):
+    """display a list of Persons if a list is provided"""
     print("\n===================================")
     for person in the_list:
         print("{name:12s}\t\t{phone}".format(name=person.name, phone=person.phone))
@@ -109,6 +144,7 @@ def display_list(the_list):
 
 
 def db_show_all():
+    """creates a list of all entry's in the database"""
     the_list = []
     db = sh.open(the_phone_book_name, flag='c', writeback=True)
     for key in db:
@@ -116,22 +152,26 @@ def db_show_all():
         person.name = key
         person.phone = db[key]
         the_list.append(person)
-
     display_list(the_list)
-    # print("{name:12s}\t\t{phone}".format(name=key, phone=db[key]))
-
     db.close()
 
 
 def get_name():
+    """get a person name and return it capitalized"""
     return input("Enter Name: ").capitalize()
+
+def get_phone_number(phone_number):
+    """get a person name and return it capitalized"""
+    return input("Enter Updated Phone Number for {phone}: ".format(phone=phone_number))
 
 
 def get_search_string():
+    """get a string to search on"""
     return input("Enter search name, or phone number: ")
 
 
 def main():
+    """the main engine"""
     action = 12
     # create_default_data()
     while action > 0:
@@ -141,14 +181,18 @@ def main():
         if action == 1:
             person = get_new_entry()
             db_add_entry(person)
-        # delete an entry
         elif action == 2:
-            delete_entry(get_name())
+            # update an entry
+
+            db_update_entry()
+        # delete an entry
         elif action == 3:
-            find_entry(get_search_string())
+            delete_entry(get_name())
         elif action == 4:
-            db_show_all()
+            find_entry(get_search_string())
         elif action == 5:
+            db_show_all()
+        elif action == 6:
             break
 
 
