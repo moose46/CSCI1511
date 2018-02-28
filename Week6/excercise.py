1e5bd96fcd18ae7e4ff7da072685b4fa8fea7c12
me <me@robertcurtiss.com>
2018-02-28 18:30:38 -0500

    Class room practice

---
+++ Week6/excercise.py
@@ -0,0 +1,20 @@
+"""
+Robert W. Curtiss
+6/26/2018
+"""
+
+def sqrt(n):
+    return n ** .5
+
+
+def exp(n, m):
+    return n ** m
+
+
+print(sqrt(4))
+print(sqrt(64))
+print(exp(4, 2))
+print(exp(5, 2))
+print(exp(5, 4))
+
+input("Press enter to continue!")
