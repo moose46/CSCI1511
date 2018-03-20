5fc07f558a741de86c56c4950bcb499ab97a17f2
me <me@robertcurtiss.com>
2018-03-20 10:58:12 -0400

    Alien is Complete

---
+++ Week8/Assignments/alien-blaster-robertcurtiss.py
@@ -0,0 +1,79 @@
+import random as rnd
+
+__author__ = 'Robert W. Curtiss'
+__project__ = 'CS1511'
+
+"""
+====================================================
+Author: Robert W. Curtiss
+Class: CSCI-1511
+    Project: CS1511
+    File: alien-blaster-robertcurtiss
+    Created: Mar, 20, 2018
+    
+    Description:
+    
+===================================================
+"""
+
+
+class LifeForm(object):
+    die_message = ''
+    blast_message = '... {} blasts ...{}! POW!!!\n'
+    def __init__(self, name):
+        self.name = name
+        self.hits = 10
+        self.alive = True
+
+    @property
+    def is_alive(self):
+        return self.alive
+
+    def blast(self,enemy):
+        print(self.blast_message.format(self.name, enemy.name))
+        enemy.hits -= 1
+        if enemy.hits <= 0:
+            print('\nHey >>>> {}   Bang@&! Pow%*@ Bop*%@, I gotcha!\n\n'.format(enemy.name).upper())
+            enemy.die()
+
+    def die(self):
+        print(self.die_message)
+        self.alive = False
+
+
+class Alien(LifeForm):
+
+    def __init__(self,name):
+        super(Alien,self).__init__(name)
+        self.die_message = """{}, the alien gasps and says, 'Oh, this is it.  This is the big one.
+              Yes, it's getting dark now.  Tell my 1.6 million larvae that I loved them...
+              Good-bye, cruel universe.'""".format(self.name)
+
+
+class Hero(LifeForm):
+    def __init__(self,name='Arnold'):
+        super(Hero,self).__init__(name)
+        self.die_message = """{}, the Hero, dies in his girlfriends arms, sobbing!, no, no, no. :(""".format(self.name)
+
+
+def main():
+    print("\t\tDeath of an Alien V2\n")
+
+    hero = Hero()
+    # hero.die()
+
+    alien = Alien(name = 'Zxbirb')
+    # alien.die()
+
+    while hero.is_alive and alien.is_alive:
+        turn = rnd.randrange(0,2)
+        if turn == 0:
+            hero.blast(alien)
+        else:
+            alien.blast(hero)
+
+main()
+
+
+
+
